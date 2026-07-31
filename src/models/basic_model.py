afd6e4ddc6798ef7da18d59798f9af7e25cfdde9
Saurab S <ajay.v.v.743@gmail.com>
2026-07-31 12:50:31 -0700

    basic_model: 4 conv blocks, 142k params, 73.6% test accuracy

--- src/models/basic_model.py
+++ src/models/basic_model.py
@@ -1,6 +1,6 @@
 from models.model import Model
 from tensorflow.keras import Sequential, layers
-from tensorflow.keras.layers.experimental.preprocessing import Rescaling
+from tensorflow.keras.layers import Rescaling
 from tensorflow.keras.optimizers import RMSprop, Adam
 
 class BasicModel(Model):
@@ -8,7 +8,6 @@ class BasicModel(Model):
 
         # Your code goes here
         # you have to initialize self.model to a keras model
-
         self.model = Sequential([
             layers.Rescaling(1./255, input_shape=input_shape),
 
@@ -21,11 +20,15 @@ class BasicModel(Model):
             layers.Conv2D(64, (3, 3), activation='relu'),
             layers.MaxPooling2D((2, 2)),
 
+            layers.Conv2D(32, (3, 3), activation='relu'),
+            layers.MaxPooling2D((2, 2)),
+
             layers.Flatten(),
             layers.Dense(64, activation='relu'),
+            layers.Dropout(0.3),
             layers.Dense(categories_count, activation='softmax'),
         ])
-    
+
     def _compile_model(self):
         # Your code goes here
         # you have to compile the keras model, similar to the example in the writeup
